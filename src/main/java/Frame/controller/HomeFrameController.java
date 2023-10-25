0afb9855c4f6d28cb1a3b1e09a99eeba5e178f63
Turing158 <15818961209@163.com>
2023-10-25 13:34:09 +0800

    创建添加菜单

--- src/main/java/Frame/controller/HomeFrameController.java
+++ src/main/java/Frame/controller/HomeFrameController.java
@@ -1,6 +1,8 @@
 package Frame.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 
 public class HomeFrameController {
@@ -8,4 +10,11 @@ public class HomeFrameController {
     VBox oldFrame;
     @FXML
     VBox home;
+    @FXML
+    ImageView head;
+    @FXML
+    VBox menu;
+    @FXML
+    Button closeMenuBtn;
+
 }
